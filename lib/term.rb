4d1c8294272496bccecfa1a5d0742a874ed6e6ca
NoahZinter <noah.zinter@gmail.com>
2021-04-09 10:28:30 -0600

    Test list courses, students in term

--- lib/term.rb
+++ lib/term.rb
@@ -6,4 +6,34 @@ class Term
     @courses = []
   end
 
+  def add_course(course)
+    @courses << course
+  end 
+
+  def list_courses 
+    @courses.map do |course| 
+      course.name 
+    end 
+  end 
+
+  def list_open_courses 
+    open_courses = @courses.select do |course| 
+      !course.full? 
+    end
+
+    open_courses.map do |course| 
+      course.name 
+    end 
+  end 
+
+  def list_all_students 
+    students = @courses.map do |course| 
+      course.students 
+    end.flatten! 
+    
+    students.map do |student| 
+      student.name 
+    end  
+  end 
+
 end 
--- spec/term_spec.rb
+++ spec/term_spec.rb
@@ -20,4 +20,85 @@ describe Term do
 
     expect(term.courses).to eq([])
   end 
+
+  describe "#add_course" do 
+    it "can add courses" do 
+      term = Term.new("Winter 2021") 
+      geography = Course.new("Geography", 2)
+      math = Course.new("Math", 3)
+      pottery = Course.new("Pottery", 3)
+      term.add_course(geography)
+      term.add_course(math)
+      term.add_course(pottery)
+
+      expect(term.courses).to eq([geography, math, pottery])
+    end
+  end
+
+  describe "#list_courses" do 
+    it 'lists courses by name' do 
+      term = Term.new("Winter 2021") 
+      geography = Course.new("Geography", 2)
+      math = Course.new("Math", 3)
+      pottery = Course.new("Pottery", 3)
+      term.add_course(geography)
+      term.add_course(math)
+      term.add_course(pottery)
+
+      expect(term.list_courses).to eq(["Geography", "Math", "Pottery"])
+    end 
+  end 
+
+  describe "#list_open_courses" do 
+    it 'returns open courses' do 
+      term = Term.new("Winter 2021") 
+      geography = Course.new("Geography", 2)
+      math = Course.new("Math", 3)
+      pottery = Course.new("Pottery", 3)
+      term.add_course(geography)
+      term.add_course(math)
+      term.add_course(pottery)
+
+      expect(term.list_open_courses).to eq(["Geography", "Math", "Pottery"])
+    end
+
+    it 'does not return full courses' do 
+      term = Term.new("Winter 2021") 
+      geography = Course.new("Geography", 2)
+      math = Course.new("Math", 3)
+      pottery = Course.new("Pottery", 3)
+      term.add_course(geography)
+      term.add_course(math)
+      term.add_course(pottery)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      geography.enroll(student1)
+      geography.enroll(student2)
+
+      expect(term.list_open_courses).to eq(["Math", "Pottery"])
+    end 
+  end 
+
+  describe "#list_all_students" do 
+    it 'lists students across all courses' do 
+      term = Term.new("Winter 2021") 
+      geography = Course.new("Geography", 2)
+      math = Course.new("Math", 3)
+      pottery = Course.new("Pottery", 3)
+      term.add_course(geography)
+      term.add_course(math)
+      term.add_course(pottery)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      student3 = Student.new({name: "Tim", age: 43})
+      student4 = Student.new({name: "Alice", age: 19})
+      geography.enroll(student1)
+      geography.enroll(student2)
+      math.enroll(student3)
+      pottery.enroll(student4)
+
+      expect(term.list_all_students).to eq(["Morgan", "Jordan", "Tim", "Alice"])
+    end
+  end 
+
 end
