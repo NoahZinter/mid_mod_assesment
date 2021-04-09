26e7e9bd24e3907f727df9082f6d22f6e59f5c54
NoahZinter <noah.zinter@gmail.com>
2021-04-08 18:58:04 -0600

    Test .enroll and .full? in course_spec

--- lib/course.rb
+++ lib/course.rb
@@ -1,10 +1,18 @@
 class Course 
 
-    attr_reader :name, :capacity, :students
-    def initialize(name, capacity)
-        @name = name 
-        @capacity = capacity 
-        @students = []
-    end 
+  attr_reader :name, :capacity, :students
+  def initialize(name, capacity)
+    @name = name 
+    @capacity = capacity 
+    @students = []
+  end 
+
+  def full? 
+    students.length == capacity
+  end 
+
+  def enroll(student)
+    @students << student
+  end 
 
 end 
--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -2,19 +2,49 @@ require './lib/course'
 require './lib/student'
 
 describe Course do 
-    it 'exists' do 
+  it 'exists' do 
+    course = Course.new("Calculus", 2)
+
+    expect(course).to be_a(Course)
+  end 
+
+  it 'has attributes' do 
+    course = Course.new("Calculus", 2)
+
+    expect(course.name).to eq("Calculus")
+    expect(course.capacity).to eq(2)
+    expect(course.students).to eq([])
+  end
+
+  describe "#full?" do 
+    it "starts as false" do 
       course = Course.new("Calculus", 2)
 
-      expect(course).to be_a(Course)
-    end 
-
-    it 'has attributes' do 
-        course = Course.new("Calculus", 2)
-
-        expect(course.name).to eq("Calculus")
-        expect(course.capacity).to eq(2)
-        expect(course.students).to eq([])
+      expect(course.full?).to eq(false)
     end
+
+    it 'is true when capacity is full' do 
+      course = Course.new("Calculus", 2)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      course.enroll(student1)  
+      course.enroll(student2)
+
+      expect(course.full?).to eq(true)
+    end 
+  end
+
+  describe "#enroll" do 
+    it "adds student to @students" do
+      course = Course.new("Calculus", 2)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      course.enroll(student1)  
+      course.enroll(student2)   
+      
+      expect(course.students).to eq([student1, student2])
+    end
+  end 
     
 
 
