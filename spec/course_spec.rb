926eaa22c7c3ca705a956c673011402ebdeb2cd6
NoahZinter <noah.zinter@gmail.com>
2021-04-08 18:47:30 -0600

    Test Course attributes

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -8,4 +8,14 @@ describe Course do
       expect(course).to be_a(Course)
     end 
 
+    it 'has attributes' do 
+        course = Course.new("Calculus", 2)
+
+        expect(course.name).to eq("Calculus")
+        expect(course.capacity).to eq(2)
+        expect(course.students).to eq([])
+    end
+    
+
+
 end 
