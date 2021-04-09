ccc0e30c17699d5dd75d8a0f41d561949fd9052e
NoahZinter <noah.zinter@gmail.com>
2021-04-08 18:44:39 -0600

    Create Course

---
+++ spec/course_spec.rb
@@ -0,0 +1,11 @@
+require './lib/course'
+require './lib/student'
+
+describe Course do 
+    it 'exists' do 
+      course = Course.new("Calculus", 2)
+
+      expect(course).to be_a(Course)
+    end 
+
+end 
